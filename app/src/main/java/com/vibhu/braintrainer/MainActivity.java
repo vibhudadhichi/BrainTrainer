87c08f65a1627b15655ef3590186654f7852dc41
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-05 00:07:54 +0530

    Random Numbers for Sum.

--- app/src/main/java/com/vibhu/braintrainer/MainActivity.java
+++ app/src/main/java/com/vibhu/braintrainer/MainActivity.java
@@ -12,7 +12,22 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
-    ArrayList<Integer> answers;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+    TextView resultTextView;
+    int locationOfCorrectAnswer;
+    int score = 0;
+
+    public void chooseAnswer(View view)
+    {
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))){
+
+            score = score + 1;
+            resultTextView.setText("Correct !");
+        }
+        else
+            resultTextView.setText("Incorrect !");
+
+    }
 
     public void start(View view){
 
@@ -28,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
         startButton = (Button) findViewById(R.id.startButton);
         TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        resultTextView = (TextView)findViewById(R.id.resultTextView);
 
         Random rand = new Random();
 
@@ -35,6 +55,30 @@ public class MainActivity extends AppCompatActivity {
         int b = rand.nextInt(21);
 
         sumTextView.setText(Integer.toString(a)+"+"+Integer.toString(b));
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        int incorrectAnswer;
+
+        for(int i = 0;i < 4; i++)
+        {
+            if(i == locationOfCorrectAnswer)
+                answers.add(a+b);
+            else
+            {
+                incorrectAnswer = rand.nextInt(41);
+                while(incorrectAnswer == a+b)
+                    incorrectAnswer = rand.nextInt(41);
+                answers.add(incorrectAnswer);
+            }
+
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+
+
 
 
     }
