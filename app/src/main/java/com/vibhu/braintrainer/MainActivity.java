ea5309ec333ae4fda0d27c052bd9cb6685fc7aa8
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-04 23:24:58 +0530

    Random Numbers for Sum.

--- app/src/main/java/com/vibhu/braintrainer/MainActivity.java
+++ app/src/main/java/com/vibhu/braintrainer/MainActivity.java
@@ -1,13 +1,41 @@
 package com.vibhu.braintrainer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    ArrayList<Integer> answers;
+
+    public void start(View view){
+
+        startButton.setVisibility(view.INVISIBLE);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a)+"+"+Integer.toString(b));
+
+
     }
 }
